75d1db0bcea71ad81e20a558a9a8038b1f5c56ea
KauanGomesFernandes <kauangomesfernandespro@gmail.com>
2024-11-07 17:12:13 -0300

    Salvando

---
+++ MauiAppHotel/Models/Hospedagem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppHotel.Models
+{
+    public class Hospedagem
+    {
+        public Quarto QuartoSelecionado { get; set; }
+        public int QntAdultos { get; set; }
+        public int QntCriancas { get; set; }
+        public DateTime DataCheckIn { get; set; }
+        public DateTime DataCheckOut { get; set; }
+
+        public int Estadias
+        {
+            get => DataCheckOut.Subtract(DataCheckIn).Days;
+        }
+
+        public double ValorTotal
+        {
+            get
+            {
+                double valor_adultos = QntAdultos * QuartoSelecionado.valorDiariaAdulto;
+                double valor_criancas = QntCriancas * QuartoSelecionado.valorDiariaCrianca;
+
+                double total = (valor_adultos + valor_criancas) * Estadias;
+
+                return total;
+            }
+        }
+    }
+}
