c832484e7cf92f41f42233ab0b4d8ae185beaecc
KauanGomesFernandes <kauangomesfernandespro@gmail.com>
2024-11-27 15:52:59 -0300

    Criação base SQLITE

---
+++ MauiAppHotel/Helpers/SQLiteDataBaseHelper.cs
@@ -0,0 +1,23 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+using SQLite;
+using MauiAppHotel.Models;
+
+namespace MauiAppHotel.Helpers
+{
+    public class SQLiteDataBaseHelper
+    {
+        readonly SQLiteAsyncConnection __conn;
+
+        public SQLiteDataBaseHelper(string path)
+        {
+            __conn = new SQLiteAsyncConnection(path);
+            __conn.CreateTableAsync<Hospedagem>().Wait();
+            __conn.CreateTableAsync<Quarto>().Wait();
+        }
+    }
+}
